460affc96bd1f74055a27c432e62f19b7f158166
GoodJeans <limguanji@gmail.com>
2020-07-29 10:55:29 +0800

    combining reducers

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,18 +1,18 @@
-import { COMMENTS } from '../shared/comments';
-import { LEADERS } from '../shared/leaders';
-import { PROMOTIONS } from '../shared/promotions';
-import { DISHES } from '../shared/dishes';
+// import { COMMENTS } from '../shared/comments';
+// import { LEADERS } from '../shared/leaders';
+// import { PROMOTIONS } from '../shared/promotions';
+// import { DISHES } from '../shared/dishes';
 
-export const initialState = {
-    dishes: DISHES,
-    comments: COMMENTS,
-    promotions: PROMOTIONS,
-    selectedDish: null,
-    leaders: LEADERS
+// export const initialState = {
+//     dishes: DISHES,
+//     comments: COMMENTS,
+//     promotions: PROMOTIONS,
+//     selectedDish: null,
+//     leaders: LEADERS
 
-};
+// };
 
-export const Reducer = (state= initialState, action) => {
-    return state;
-};
+// export const Reducer = (state= initialState, action) => {
+//     return state;
+// };
 
